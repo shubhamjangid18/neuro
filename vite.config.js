ba9b88fe8b25ac879b72e6d35550043cc3d3b62d
Shubham Jangid <shubhamjangid1818@gmail.com>
2026-07-30 16:06:27 +0530

    chore: opt out of experimental Rolldown, use Rollup for build stability

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    experimental: {
+      // opt out of Rolldown and use Rollup instead for more stable builds
+      rollup: true
+    }
+  }
 })
